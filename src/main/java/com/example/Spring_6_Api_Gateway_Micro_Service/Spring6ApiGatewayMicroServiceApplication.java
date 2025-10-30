2e5fc0b35004090bed1bbeadd7f2ed24a3f2ecda
Devinder <devinderwadhwa20@gmail.com>
2025-10-30 19:59:57 +0530

    Added Dependencies and README.md file and config in application.properties

---
+++ src/main/java/com/example/Spring_6_Api_Gateway_Micro_Service/Spring6ApiGatewayMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_6_Api_Gateway_Micro_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6ApiGatewayMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6ApiGatewayMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring_6_Api_Gateway_Micro_Service/Spring6ApiGatewayMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_6_Api_Gateway_Micro_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6ApiGatewayMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
